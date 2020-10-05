7399f703d0cf85a45122903dfe017584f3f609b6
Sasank Sunkavalli <sasanksais@zenoti.com>
2020-10-05 12:40:54 +0530

    Converted Base code to a Stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(BaseApp());
+}
+
+class BaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: Container(),
       ),
-    ),
-  );
+    );
+  }
 }
